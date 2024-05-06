a6ed8acbbfdb7514ae4df29da2c3c9fd0c0de54c
sarakord <s.kordtd@gmail.com>
2024-05-06 15:48:46 +0330

    added student service logic code

--- src/main/java/com/insert/university/services/StudentService.java
+++ src/main/java/com/insert/university/services/StudentService.java
@@ -2,8 +2,26 @@ package com.insert.university.services;
 
 import com.insert.university.model.entities.StudentEntity;
 import com.insert.university.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService extends BaseService<StudentEntity,StudentRepository> {
+
+private  StudentEntity studentEntity;
+
+    public StudentEntity createAccount(String studentName, String studentFamily,String studentNationalCode ) {
+        if (studentName == null || studentFamily == null|| studentNationalCode==null) {
+            throw new IllegalArgumentException("نام دانشجو و ایمیل نمی‌تواند خالی باشد");
+        }
+
+        studentEntity.setName(studentName);
+        studentEntity.setFamily(studentFamily);
+        studentEntity.setNationalCode(studentNationalCode);
+
+
+        repository.save(studentEntity);
+
+        return studentEntity;
+    }
 }
